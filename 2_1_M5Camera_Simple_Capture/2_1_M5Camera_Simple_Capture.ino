9a8b70e570ea566458f48cc03da5421b31786319
anoken <31365453+anoken@users.noreply.github.com>
2019-07-18 00:19:56 +0900

    Update 2_1_M5Camera_Simple_Capture.ino

--- 2_1_M5Camera_Simple_Capture/2_1_M5Camera_Simple_Capture.ino
+++ 2_1_M5Camera_Simple_Capture/2_1_M5Camera_Simple_Capture.ino
@@ -14,49 +14,54 @@ const char* password = "your_passwd";
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  if (!SPIFFS.begin(true)) {
+  if (!SPIFFS.begin(true)) {            //SPIFFSの開始
     Serial.println("SPIFFS Mount Failed");
   }
-  setup_camera();
+  setup_camera();                       //M5Cameraの設定
 }
 void loop() {
-  capture_camera();
+  capture_camera();                     //M5Cameraの撮影
   delay(1000);
 }
-
-void setup_camera() {
+void setup_camera() {		         //M5Cameraの設定
   camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 6;
-  config.fb_count = 1;
+
+  //M5Camera GPIO 
+  config.ledc_channel = LEDC_CHANNEL_0;	/*!< LEDC channel generating XCLK  */
+  config.ledc_timer = LEDC_TIMER_0;	    /*!< LEDC timer generating XCLK  */
+  config.pin_d0 = Y2_GPIO_NUM;	 	      /*!< GPIO pin D0 line */
+  config.pin_d1 = Y3_GPIO_NUM;	 	      /*!< GPIO pin D1 line */
+  config.pin_d2 = Y4_GPIO_NUM;	 	      /*!< GPIO pin D2 line */
+  config.pin_d3 = Y5_GPIO_NUM;	 	      /*!< GPIO pin D3 line */
+  config.pin_d4 = Y6_GPIO_NUM;	 	      /*!< GPIO pin D4 line */
+  config.pin_d5 = Y7_GPIO_NUM;	 	      /*!< GPIO pin D5 line */
+  config.pin_d6 = Y8_GPIO_NUM;	 	      /*!< GPIO pin D6 line */
+  config.pin_d7 = Y9_GPIO_NUM;	 	      /*!< GPIO pin D7 line */
+  config.pin_xclk = XCLK_GPIO_NUM;	    /*!< GPIO pin XCLK line */
+  config.pin_pclk = PCLK_GPIO_NUM;	    /*!< GPIO pin PCLK line */
+  config.pin_vsync = VSYNC_GPIO_NUM;	  /*!< GPIO pin VSYNC line */
+  config.pin_href = HREF_GPIO_NUM;	    /*!< GPIO pin HREF line */
+  config.pin_sscb_sda = SIOD_GPIO_NUM;	/*!< GPIO pin SDA line */
+  config.pin_sscb_scl = SIOC_GPIO_NUM;	/*!< GPIO pin SCL line */
+  config.pin_pwdn = PWDN_GPIO_NUM;	    /*!< GPIO pin power down line */
+  config.pin_reset = RESET_GPIO_NUM;	  /*!< GPIO pin reset line */
+  config.xclk_freq_hz = 20000000;	      /*!< Frequency of XCLK signal*/
+
+  //M5Camera Image Format
+  config.pixel_format = PIXFORMAT_JPEG;	/*!< Format of the pixel data*/
+  config.frame_size = FRAMESIZE_VGA;	  /*!<imageSize*/
+  config.jpeg_quality = 6;		          /*!< JPEG Quality range of 0-63 */
+  config.fb_count = 1;	                /*!< Number of frame buffers to be allocated.  */
+
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
   sensor_t * s = esp_camera_sensor_get();
-  auto st = s->status;
+
 }
+
 
 void capture_camera() {
   uint32_t data_len = 0;
